c01257a8ffdfd2f66df55af1361daa44d2dc1b9e
Caroline <hicaarol@gmail.com>
2019-06-09 23:37:44 -0300

    change client.py

--- client/client.py
+++ client/client.py
@@ -1,14 +1,27 @@
 import requests
+import os
+import time
 
-content = { 'time':'10:20',
-            'name':'teste',
-            'id_cont':'1',
-            'id_img':'21',
-            'CPU':'1',
-            'Mem':'2048'}
+def get_info():
+
+    command = 'docker run -it -v /var/run/docker.sock:/var/run/docker.sock docker stats --no-stream  --format "table #{{.Container}}#{{.Name}}#{{.CPUPerc}}#{{.MemUsage}}"'
+
+    info = os.popen(command).read()
+
+    info = info.split('#')[5:] #remove column names
+
+    content = {
+        'time': time.time(),
+        'id_cont': info[0],
+        'name': info[1],
+        'CPU': info[2],
+        'Mem': info[3]
+    }
+
+    return content
 
 def run():
-    
+
     while(1):
 
         print('''
@@ -16,12 +29,13 @@ def run():
             2. Enviar GET
             3. Finalizar processo
         ''')
+
         option = input("Digite a opcao: ")
 
         if(option == '1'):
-            r = requests.post('http://http://192.168.33.10:5001', json=content)
-            print(r.status_code)
-            print(r.content)
+            response = requests.post('http://http://192.168.33.10:5001', json=get_info())
+            print(response.status_code)
+            print(response.content)
 
         elif(option == '2'):
             response = requests.get('http://192.168.33.10:5001')
@@ -31,5 +45,7 @@ def run():
         else:
             exit()
 
-if __name__ == "__main__":
+
+if __name__ == "__main__": 
     run()
+
